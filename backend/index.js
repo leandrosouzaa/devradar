d47d14d422781bcd448ae48d0d2c786252801a55
leandrosouzaa <leandror1.souza@hotmail.com>
2020-01-14 15:15:38 -0300

    Conexão com o MongoDB

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://leandro:omnistack@devradar-lmhxy.mongodb.net/week10?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 app.use(express.json());
 
