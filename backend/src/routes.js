1cc71d8653c13c22e326de77b4bde00a595a0f15
leandrosouzaa <leandror1.souza@hotmail.com>
2020-01-14 16:06:56 -0300

    Adição da localização no cadastro de devs e validação para duplo registro.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,28 +1,11 @@
 const { Router } = require('express');
-const axios = require('axios');
-const Dev = require('./models/Dev');
+
+
+const DevController = require('./controllers/DevController');
 
 const routes = Router();
 
-routes.post('/devs', async (req, res) => {
-  const { github_username, techs } = req.body;
-
-  const response = await axios.get(`https://api.github.com/users/${github_username}`);
-
-  const { name = login, avatar_url, bio } = response.data;
-
-  const techsArray = techs.split(' ,').map((tech) => tech.trim());
-
-  const dev = await Dev.create({
-    github_username,
-    name,
-    avatar_url,
-    bio,
-    techs: techsArray,
-  });
-
-  return res.json(dev);
-});
+routes.post('/devs', DevController.store);
 
 
 module.exports = routes;
