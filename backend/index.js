dfd7eee5c0a0546d68574bf59cc321b7de017f0e
leandrosouzaa <leandror1.souza@hotmail.com>
2020-01-14 14:45:22 -0300

    Tipos de parâmetros

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,17 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+/**
+ * Tipos de parâmetros: req.query (filtros, paginação, ordenação...)
+ * Query Params: req.params  (Identificar um recurso na alteração ou remoção)
+ * Route Params: req.body (Dados para criação ou alteração de um registro.)
+ * Body:
+ */
+
 app.get('/', (req, res) => res.json('Olá Mundo'));
 
 app.listen(3333);
