8a8464ad8afbaeea6f37b45ae9cbc09442f8bc37
leandrosouzaa <leandror1.souza@hotmail.com>
2020-01-14 17:07:46 -0300

    Criação da feature de busca de usuários.

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,8 +1,14 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
-
+const parseStringAsArray = require('../utils/parseStringAsArray');
 
 module.exports = {
+  async index(req, res) {
+    const devs = await Dev.find();
+
+    return res.json(devs);
+  },
+
   async store(req, res) {
     const {
       github_username, techs, latitude, longitude,
@@ -18,8 +24,8 @@ module.exports = {
 
     const { name = login, avatar_url, bio } = response.data;
 
-    const techsArray = techs.split(' ,').map((tech) => tech.trim());
-
+    const techsArray = parseStringAsArray(techs);
+    console.log(techsArray);
     const location = {
       type: 'Point',
       coordinates: [longitude, latitude],
