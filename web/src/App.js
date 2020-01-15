c2fd283e0779e5631e4681125082839586cfe073
leandrosouzaa <leandror1.souza@hotmail.com>
2020-01-15 16:10:07 -0300

    Conhecendo conceitos do React

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+  // Componente: Bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação.
+  // Estado: Informações contidas em um componente (lembrar: imutabilidade)
+  // Propriedade: Informações que passam de um componente PAI para um componente FILHO.
 
 function App() {
+
+  const [counter, setCounter] = useState(0)
+
+  function incrementCounter() {
+    setCounter(counter++)
+  }
+
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Contador: </h1>
+      <button onClick={incrementCounter}>Incrementar</button>
+    </>
   );
 }
 
